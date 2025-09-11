477309d88d5f2c7c0a6c613067b06aa8630207ae
seoik <seoikk21@gmail.com>
2025-09-12 01:57:52 +0900

    feat : 유효성 검사 focus 관련 개발

--- app/sign/sign-form.tsx
+++ app/sign/sign-form.tsx
@@ -1,12 +1,11 @@
 "use client";
-
 import { LoaderPinwheelIcon } from "lucide-react";
 import Link from "next/link";
 import { useActionState, useReducer } from "react";
 import z from "zod";
 import LabelInput from "@/components/label-input";
 import { Button } from "@/components/ui/button";
-import type { ValidError } from "@/lib/validator";
+import type { ValidErrorObject } from "@/lib/validator";
 import { authorize } from "./sign.action";
 
 export default function SignForm() {
@@ -24,64 +23,61 @@ export default function SignForm() {
 
 function SignIn({ toggleSign }: { toggleSign: () => void }) {
   const makeLogin = async (formData: FormData) => {
-    // const email = formData.get('email');
-    // const passwd = formData.get('passwd');
+    // const email = formData.get("email");
+    // const passwd = formData.get("passwd");
 
     // const validator = z
     //   .object({
-    //     email: z.email('잘못된 이메일 형식입니다!'),
-    //     passwd: z.string().min(6, 'More than 6 characters!'),
+    //     email: z.email("잘못된 이메일 형식입니다."),
+    //     passwd: z.string().min(6),
     //   })
     //   .safeParse(Object.fromEntries(formData.entries()));
 
     // if (!validator.success) {
-    //   console.log('Error:', validator.error);
+    //   console.log("🚀 ~ validator.error:", validator.error);
     //   return alert(validator.error);
     // }
 
-    await authorize(formData);
+    const res = await authorize(formData);
+    console.log("🚀 ~ res sign form -----------> ", res);
   };
 
   return (
     <>
-      <form action={makeLogin} className="flex flex-col space-y-3">
+      <form action={makeLogin} className="flex flex-col space-y-2">
         <LabelInput
           label="email"
-          type="email"
           name="email"
+          type="email"
           defaultValue={"jeonseongho@naver.com"}
           placeholder="email@bookmark.com"
         />
-
         <LabelInput
           label="password"
-          type="password"
           name="passwd"
-          defaultValue={"121212"}
-          placeholder="your password.."
-          className="my-3x"
+          type="password"
+          defaultValue={"11111111"}
+          placeholder="Your Password"
         />
-
         <div className="flex justify-between">
           <label htmlFor="remember" className="cursor-pointer">
             <input
               type="checkbox"
               id="remember"
+              defaultChecked={true}
               className="mr-1 translate-y-[1px]"
             />
             Remember me
           </label>
-
           <Link href="#">Forgot Password?</Link>
         </div>
-
         <Button type="submit" variant={"primary"} className="w-full">
           Sign In
         </Button>
       </form>
       <div className="mt-5 flex gap-10">
-        <span>Dont&apos;t have Account?</span>
-        <Link onClick={toggleSign} href="#">
+        <span>Don&apos;t have account?</span>
+        <Link href="#" onClick={toggleSign}>
           Sign Up
         </Link>
       </div>
@@ -91,79 +87,112 @@ function SignIn({ toggleSign }: { toggleSign: () => void }) {
 
 function SignUp({ toggleSign }: { toggleSign: () => void }) {
   const [validError, makeRegist, isPending] = useActionState(
-    async (_preValidError: ValidError | undefined, formData: FormData) => {
+    async (
+      _preValidError: ValidErrorObject | undefined,
+      formData: FormData
+    ) => {
       const validator = z
         .object({
           email: z.email(),
-          passwd: z.string().min(6),
-          passwd2: z.string().min(6),
-          nickname: z.string().min(3),
+          passwd: z.string().min(1),
+          passwd2: z.string().min(1),
+          nickname: z.string().min(1),
         })
-        .refine(
-          ({ passwd, passwd2 }) => passwd === passwd2,
-          "Passwords are not matched!"
-        )
+        .refine(({ passwd, passwd2 }) => passwd === passwd2, {
+          error: "비밀번호가 일치하지 않습니다.",
+          path: ["passwd2"],
+        })
         .safeParse(Object.fromEntries(formData.entries()));
 
       if (!validator.success) {
-        const err = z.treeifyError(validator.error).properties;
-        return err;
+        // const err1 = validator.error.flatten();
+        const valid = validator.error;
+        const tree = z.treeifyError(valid);
+
+        // console.log("🚀 ~ validator:", validator);
+        // console.log("🚀 ~ tree:", tree);
+
+        const errObject: ValidErrorObject = {
+          prop: tree.properties,
+          path: valid.issues[0].path[0] as string,
+        };
+
+        return errObject;
       }
     },
     undefined
   );
 
+  // const makeRegist = async (formData: FormData) => {
+  //   const validator = z
+  //     .object({
+  //       email: z.email(),
+  //       passwd: z.string().min(6),
+  //       passwd2: z.string().min(6),
+  //       nickname: z.string().min(3),
+  //     })
+  //     .refine(
+  //       ({ passwd, passwd2 }) => passwd === passwd2,
+  //       "비밀번호가 일치하지 않습니다."
+  //     )
+  //     .safeParse(Object.fromEntries(formData.entries()));
+
+  //   if (!validator.success) {
+  //     // const err1 = validator.error.flatten();
+  //     const err2 = z.treeifyError(validator.error).properties;
+  //     console.log("🚀 ~ err2:", err2);
+  //   }
+  // };
   return (
     <>
-      <form action={makeRegist} className="flex flex-col space-y-2">
+      <form action={makeRegist} className="flex flex-col space-y-3">
         <LabelInput
           label="email"
           type="email"
           name="email"
+          focus={true}
           error={validError}
+          // defaultValue={"jeonseongho@naver.com"}
           placeholder="email@bookmark.com"
         />
-
         <LabelInput
           label="password"
           type="password"
           name="passwd"
+          focus={true}
           error={validError}
-          placeholder="your password.."
-          className="my-3x"
+          placeholder="Your Password..."
         />
-
         <LabelInput
           label="password confirm"
           type="password"
           name="passwd2"
+          focus={true}
           error={validError}
-          placeholder="your password.."
-          className="my-3x"
+          placeholder="Your Password..."
         />
-
         <LabelInput
           label="nickname"
           type="text"
           name="nickname"
+          focus={true}
           error={validError}
-          placeholder="your nickname.."
-          className="my-3x"
+          // defaultValue={"111"}
+          placeholder="Your NickName..."
         />
-
         <Button
           type="submit"
           variant={"primary"}
           className="w-full"
           disabled={isPending}
         >
-          {isPending ? "Singing Up..." : "Sign Up"}
+          {/* {isPending ? "Singing Up..." : "Sign Up"} */}
           {isPending && <LoaderPinwheelIcon className="animate-spin" />} Sign Up
         </Button>
       </form>
       <div className="mt-5 flex gap-10">
-        <span>Already have Account?</span>
-        <Link onClick={toggleSign} href="#">
+        <span>Already have account</span>
+        <Link href="#" onClick={toggleSign}>
           Sign In
         </Link>
       </div>
--- components/label-input.tsx
+++ components/label-input.tsx
@@ -1,5 +1,4 @@
 "use client";
-
 import {
   type ComponentProps,
   type RefObject,
@@ -8,57 +7,63 @@ import {
   useRef,
 } from "react";
 import { cn } from "@/lib/utils";
-import type { ValidError } from "@/lib/validator";
+import type { ValidErrorObject } from "@/lib/validator";
 import { Input } from "./ui/input";
 
 type Props = {
   label: string;
   type?: string;
   name?: string;
-  ref?: RefObject<HTMLInputElement | null>;
   focus?: boolean;
-  error?: ValidError;
+  error?: ValidErrorObject;
   placeholder?: string;
   className?: string;
+  ref?: RefObject<HTMLInputElement | null>;
 };
-
 export default function LabelInput({
   label,
   type,
   name,
-  ref,
   focus,
   error,
   placeholder,
   className,
+  ref,
   ...props
 }: Props & ComponentProps<"input">) {
-  const uniqName = useId();
+  const uniqueName = useId();
+  // if (error?.prop) console.log(name, error.prop);
+  const err =
+    !!error?.prop && name && error.prop[name] ? error.prop[name].errors : [];
+  const path = error?.path;
   const inpRef = useRef<HTMLInputElement>(null);
-  const err = !!error && !!name && error[name] ? error[name].errors : [];
 
   useEffect(() => {
-    if (!focus && !err.length) return;
-    if (ref) ref.current?.focus();
-    else inpRef.current?.focus();
-  }, [focus, ref, err]);
+    if (!focus || !err.length) return;
+    if (ref) {
+      ref.current?.focus();
+    } else {
+      // console.log("path", path, inpRef.current);
+      if (inpRef.current?.name === path) inpRef.current?.focus();
+    }
+  }, [focus, err, ref, path]);
 
   return (
-    <label htmlFor={uniqName} className="font-semibold text-sm capitalize">
+    <label htmlFor={uniqueName} className="font-semibold text-sm capitalize">
       {label}
       <Input
         type={type || "text"}
-        id={uniqName}
-        name={name || uniqName}
+        id={uniqueName}
+        name={name || uniqueName}
         ref={ref || inpRef}
         placeholder={placeholder || ""}
         className={cn("bg-gray-100 font-normal focus:bg-white", className)}
         {...props}
       />
       {err.map((e) => (
-        <small key={e} className="ml-1 text-red-400">
+        <span key={e} className="ml-1 font-medium text-red-400">
           {e}
-        </small>
+        </span>
       ))}
     </label>
   );
